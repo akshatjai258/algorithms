add38c624ee686b131d783ea0d2f56a814fd8544
Lareen Melo <lareenmelo@gmail.com>
2020-01-20 00:56:54 -0500

    Add insert at method on swift singly linked list, also added count and fixed some error handling

--- implementations/data structures/linked lists/singly/singly_linked_list.swift
+++ implementations/data structures/linked lists/singly/singly_linked_list.swift
@@ -19,9 +19,12 @@ class Node<T> {
 
 class LinkedList<T> {
     var head: Node<T>?
+    var count: Int
     
-    // create count
-    
+    init() {
+        count = 0
+    }
+        
     /// inserts new node at the last of linked list
     func append(_ value: T) {
         var current = head
@@ -37,15 +40,45 @@ class LinkedList<T> {
             
             current?.next = newNode
         }
+        
+        count += 1
     }
     
     /// inserts new node at specified position, "shifting" the rest of the elements to the right.
+    func insert(_ value: T, at position: Int) {
+        var current = head
+        let newNode = Node(value)
+        var pos = position
+                
+        assert(position >= 0, "Invalid Index")
+        
+        if position == 0 {
+            newNode.next = current
+            head = newNode
+            count += 1
+        } else if position == count - 1 {
+            append(value)
+            
+        } else {
+            assert(position < count, "Invalid Index")
+
+            while current != nil && pos > 1 {
+                current = current?.next
+                pos -= 1;
+            }
+            
+            newNode.next = current?.next
+            current?.next = newNode
+
+            count += 1
+        }
+    }
     
     /// removes last node from linked list
     func remove() {
         var current = head
         
-        assert(current != nil)
+        assert(current != nil, "There are no nodes to delete!")
         
         if current?.next == nil {
             head = nil
@@ -60,10 +93,17 @@ class LinkedList<T> {
             predecessor?.next = nil
             current = nil
         }
+        
+        count -= 1
     }
     
     /// removes nodes from specified position, "shifting" the rest of the elements to the left.
+    func removeNode(at position: Int) {
+        
+    }
     
+    
+    // FIXME: add pretty priting prettily
     func prettyPrint() {
         var currentNode = head
         
@@ -83,5 +123,8 @@ list.append(3)
 list.remove()
 list.remove()
 list.remove()
-list.remove()
+list.insert(8, at: 0)
+list.append(11)
+
+
 list.prettyPrint()
