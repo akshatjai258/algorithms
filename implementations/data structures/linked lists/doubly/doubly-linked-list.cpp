7871bd5601f442b98061e7d86d2462cbd1a6e6fd
Lareen Melo <lareenmelo@gmail.com>
2020-01-23 23:11:44 -0500

    Add Double Linked List cpp implementation

--- implementations/data structures/linked lists/doubly/doubly-linked-list.cpp
+++ implementations/data structures/linked lists/doubly/doubly-linked-list.cpp
@@ -25,6 +25,19 @@ public:
         head = nullptr;
     }
     
+    int count() {
+        int count = 0;
+        
+        Node<T> *current_node = head;
+        
+        while (current_node != nullptr) {
+            current_node = current_node->next;
+            count++;
+        }
+        
+        return count;
+    }
+    
     void append(T value) {
         Node<T> *current_node = head;
         
@@ -46,7 +59,50 @@ public:
         }
     }
     
-    // insert at
+    void insert_at(T value, int position) {
+        int number_of_nodes = count();
+        Node<T> *current_node = head;
+        
+        Node<T> *new_node = new Node<T>;
+        new_node->value = value;
+        new_node->previous = nullptr;
+        new_node->next = nullptr;
+        
+        // position needs to be greater or equal than 0 (valid index)
+        assert(position >= 0);
+        
+        if (position == 0) {
+            if (current_node == nullptr) {
+                head = new_node;
+                delete current_node;
+                current_node = nullptr;
+                
+            } else {
+                new_node->next = current_node;
+                current_node->previous = new_node;
+                head = new_node;
+            }
+            
+        } else if (position == number_of_nodes) {
+            append(value);
+            
+        } else {
+            // index needs the be smaller than number of nodes.
+            assert(position < number_of_nodes);
+            
+            while (current_node->next != nullptr && position--) {
+                current_node = current_node->next;
+            }
+            
+            Node<T> *previous_node = current_node->previous;
+            
+            previous_node->next = new_node;
+            current_node->previous = new_node;
+            
+            new_node->next = current_node;
+            new_node->previous = previous_node;
+        }
+    }
     
     void remove() {
         Node<T> *current_node = head;
@@ -74,7 +130,56 @@ public:
         }
     }
     
-    // delete at
+    void remove_at(int position) {
+        int number_of_nodes = count();
+        Node<T> *current_node = head;
+        
+        // position needs to be greater or equal than 0 (valid index)
+        assert(position >= 0);
+        
+        if (position == number_of_nodes - 1) {
+            remove();
+            
+        } else if (position == 0) {
+            
+            if (current_node->next == nullptr) {
+                head = nullptr;
+                delete current_node;
+                current_node = nullptr;
+                
+            } else {
+                head = current_node->next;
+                current_node->next->previous = nullptr;
+                current_node->next = nullptr;
+                
+                delete current_node;
+                
+                current_node = nullptr;
+            }
+            
+        } else {
+            // index needs the be smaller than number of nodes.
+            assert(position < number_of_nodes);
+            
+            while (current_node->next != nullptr && position--) {
+                current_node = current_node->next;
+            }
+            
+            Node<T> *previous_node = current_node->previous;
+            Node<T> *next_node = current_node->next;
+            
+            previous_node->next = next_node;
+            next_node->previous = previous_node;
+            
+            current_node->next = nullptr;
+            current_node->previous = nullptr;
+            
+            delete current_node;
+            
+            current_node = nullptr;
+            
+        }
+    }
     
     void print() {
         Node<T> *current_node = head;
@@ -87,22 +192,17 @@ public:
             
             current_node = current_node->next;
         }
+        
+        std::cout << std::endl;
     }
 };
 
 int main() {
     DoublyLinkedList<int> list;
     
-    list.append(5);
-    list.append(6);
-    list.append(7);
+    list.append(1);
+    list.append(3);
+    list.insert_at(2, 1);
     
-    list.remove();
-    list.remove();
-    list.remove();
-
     list.print();
-    std::cout << std::endl;
-    
-    
 }
